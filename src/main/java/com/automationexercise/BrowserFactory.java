c9947a282675d14fdda8b1fbb99163f816e1db6a
Oleksii <bovsunovskiyas@gmail.com>
2025-07-11 19:20:34 +0300

    Init UI project and part of API test-cases

--- src/main/java/com/automationexercise/BrowserFactory.java
+++ src/main/java/com/automationexercise/BrowserFactory.java
@@ -9,7 +9,7 @@ public class BrowserFactory {
     public WebDriver getWebDriverInstance(){
 
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless"); // Запуск без графічного інтерфейсу
+        options.addArguments("--headless"); // Запуск без графічного інтерфейсу
         options.addArguments("--no-sandbox"); // Необхідно для запуску в Docker/Linux контейнерах
         options.addArguments("--disable-dev-shm-usage"); // Уникаємо проблем з пам'яттю в контейнерах
         options.addArguments("--window-size=1920,1080"); // Встановлюємо розмір вікна
