e90061d9390b66cfc91452a5c36730b622edc2e8
Oleksii <bovsunovskiyas@gmail.com>
2025-08-13 17:19:49 +0300

    Check fail scenario with screenshots

--- src/test/java/com/automationexercise/ui/BaseTest.java
+++ src/test/java/com/automationexercise/ui/BaseTest.java
@@ -3,11 +3,19 @@ package com.automationexercise.ui;
 import com.automationexercise.BrowserFactory;
 import com.automationexercise.helpers.Browser;
 import com.automationexercise.pages.BasePage;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+
+import java.io.ByteArrayInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class BaseTest {
 
@@ -23,12 +31,38 @@ public abstract class BaseTest {
 
     @AfterMethod(alwaysRun = true)
 //    @Step("Close browser after each test")
-    public void closeBrowser() {
-        BasePage.getDriver().quit();
+    public void closeBrowser(ITestResult result) {
+//        BasePage.getDriver().quit();
 //        if (BasePage.getThreadLocalDriver() != null) {
 //            BasePage.getDriver().quit();
 //        }
 
+//
+//    }
+//}
+
+        String pattern = "dd.MM.yyyy_HH-mm-ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        String timestampString = sdf.format(new Date());
+
+        if (result.getStatus() == ITestResult.FAILURE && BasePage.getDriver() != null) {
+            byte[] screenshot = ((TakesScreenshot) BasePage.getDriver()).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment(
+                    "Allure_" + timestampString + "_" + result.getName(),
+                    "image/png",
+                    new ByteArrayInputStream(screenshot),
+                    ".png"
+            );
+
+            String pageSource = BasePage.getDriver().getPageSource();
+            Allure.addAttachment("Page Source", "text/html", pageSource, ".html");
+
+        }
+
+        if (BasePage.getDriver() != null) {
+            BasePage.getDriver().quit();
+            BasePage.setDriver(null); // очищаємо ThreadLocal
+        }
     }
 }
 
